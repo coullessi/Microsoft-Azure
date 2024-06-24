5b06e37368575ea9ce08fdc6e8ac2ab9b57907ed
Lessi Coulibaly <coullessi@hotmail.com>
2024-06-24 07:07:25 -0700

    Update files

--- AzureArc/Linux/PSScripts/create_service_principal.ps1
+++ AzureArc/Linux/PSScripts/create_service_principal.ps1
@@ -1,6 +1,6 @@
 Clear-Host
 
-# 1. CONNECT TO AZURE
+# 1. Connect to Azure
 Write-Host "Connecting to Azure..." -ForegroundColor Yellow
 Connect-AzAccount -WarningAction SilentlyContinue | Out-Null
 $subs = @()
